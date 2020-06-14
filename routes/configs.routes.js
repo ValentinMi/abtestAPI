bdb7f33b21cac7bf6936da4aaf00680c3bd17939
Valentin Misiaszek <mowerv6@gmail.com>
2020-06-15 00:09:25 +0200

    feat(routes): add try catch blocks

--- routes/configs.routes.js
+++ routes/configs.routes.js
@@ -3,40 +3,56 @@ const { validate, Config } = require("../models/config.model");
 
 // GET -- Get all configs
 router.get("/", async (req, res) => {
-  const configs = await Config.find();
-  if (!configs || configs.length === 0)
-    return res.status(404).send("No configs found");
+  try {
+    const configs = await Config.find();
+    if (!configs || configs.length === 0)
+      return res.status(404).send("No configs found");
 
-  res.send(configs);
+    res.send(configs);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // GET -- Get a config with id
 router.get("/:id", async (req, res) => {
-  const config = await Config.findById(req.params.id);
-  if (!config) return res.status(404).send("Config not found");
+  try {
+    const config = await Config.findById(req.params.id);
+    if (!config) return res.status(404).send("Config not found");
 
-  res.send(config);
+    res.send(config);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // POST -- Create a new config
 router.post("/", async (req, res) => {
-  // Validate
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send("Bad request");
+  try {
+    // Validate
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send("Bad request");
 
-  const newConfig = new Config(req.body);
+    const newConfig = new Config(req.body);
 
-  await newConfig.save();
+    await newConfig.save();
 
-  res.send(newConfig);
+    res.send(newConfig);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // DELETE -- Delete a config with id
 router.delete("/:id", async (req, res) => {
-  const config = Config.findByIdAndRemove(req.params.id);
-  if (!config) return res.status(404).send("Confit not found");
+  try {
+    const config = Config.findByIdAndRemove(req.params.id);
+    if (!config) return res.status(404).send("Confit not found");
 
-  res.send(config);
+    res.send(config);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 module.exports = router;
--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -3,46 +3,62 @@ const { validate, Page } = require("../models/page.model");
 
 // GET -- Get a page with id
 router.get("/:id", async (req, res) => {
-  // Validation
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send("Bad request");
+  try {
+    // Validation
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send("Bad request");
 
-  const page = await Page.findById(req.params.id);
-  if (!page) return res.status(404).send("Page not found");
+    const page = await Page.findById(req.params.id);
+    if (!page) return res.status(404).send("Page not found");
 
-  res.send(page);
+    res.send(page);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // POST -- Create a new page
 router.post("/", async (req, res) => {
-  // Validation
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send("Bad request");
+  try {
+    // Validation
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send("Bad request");
 
-  const newPage = new Page(req.body);
+    const newPage = new Page(req.body);
 
-  await newPage.save();
-  res.send(newPage);
+    await newPage.save();
+    res.send(newPage);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // PUT --  Edit a page with id
 router.put("/:id", async (req, res) => {
-  // Validation
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send("Bad request");
+  try {
+    // Validation
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send("Bad request");
 
-  const page = await Page.findByIdAndUpdate(req.params.id, req.body);
-  if (!page) return res.status(404).send("Page not found");
+    const page = await Page.findByIdAndUpdate(req.params.id, req.body);
+    if (!page) return res.status(404).send("Page not found");
 
-  res.send(page);
+    res.send(page);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // DELETE -- Delete a page with id
 router.delete("/:id", async (req, res) => {
-  const page = await Page.findByIdAndRemove(req.params.id);
-  if (!page) return res.status(404).send("Page not found");
+  try {
+    const page = await Page.findByIdAndRemove(req.params.id);
+    if (!page) return res.status(404).send("Page not found");
 
-  res.send(page);
+    res.send(page);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 module.exports = router;
